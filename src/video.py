3b6ee4c8143778d0065a0d6bc24ef86ec5a8a67b
yury-yury <yury_yury@mail.ru>
2023-06-29 20:17:30 +0300

    home_work_6

--- src/video.py
+++ src/video.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
-from typing import Dict, Any
-
+from typing import Dict, Any, Optional
+# from googleapiclient.errors import HttpError
 import isodate
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
@@ -9,6 +9,10 @@ from dotenv import load_dotenv
 from src.channel import printj
 
 load_dotenv()
+
+
+class HttpError(Exception):
+    pass
 
 
 class Video:
@@ -25,15 +29,26 @@ class Video:
 
     def __init__(self, video_id: str) -> None:
         self.video_id: str = video_id
-        _video_info: Dict[str, Any] = self.get_info()
-        self.title: str = _video_info["items"][0]["snippet"]["title"]
-        self.url: str = f'https://youtu.be/{self.video_id}'
-        self.viewers: int = int(_video_info["items"][0]["statistics"]["viewCount"])
-        self.likes: int = int(_video_info["items"][0]["statistics"]["likeCount"])
-        self.duration: datetime = isodate.parse_duration(_video_info['items'][0]['contentDetails']['duration'])
+        try:
+            _video_info: Dict[str, Any] = self.get_info()
+        except HttpError:
+            self.title: Optional[str] = None
+            self.url: Optional[str] = None
+            self.viewers: Optional[int] = None
+            self.like_count: Optional[int] = None
+            self.duration: Optional[datetime] = None
+        else:
+            self.title: Optional[str] = _video_info["items"][0]["snippet"]["title"]
+            self.url: Optional[str] = f'https://youtu.be/{self.video_id}'
+            self.viewers: Optional[int] = int(_video_info["items"][0]["statistics"]["viewCount"])
+            self.like_count: Optional[int] = int(_video_info["items"][0]["statistics"]["likeCount"])
+            self.duration: Optional[datetime] = isodate.parse_duration(_video_info['items'][0]['contentDetails']['duration'])
 
     def get_info(self) -> dict:
-        return self.youtube.videos().list(id=self.video_id, part='contentDetails,snippet,statistics').execute()
+        response = self.youtube.videos().list(id=self.video_id, part='contentDetails,snippet,statistics').execute()
+        if len(response['items']) == 0:
+            raise HttpError
+        return response
 
     def __str__(self):
         return f"{self.title}"
